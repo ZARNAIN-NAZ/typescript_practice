3997c8249f6361d8543d719b248f32d7e9addc5d
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-01 16:06:18 +0500

    classes

--- project_practice/src/index.ts
+++ project_practice/src/index.ts
@@ -1,2 +1,13 @@
-console.log("typescript is her e")
-console.log("typescript is amzaing")
+// console.log("typescript is her e")
+// console.log("typescript is amzaing")
+
+//CLASSES--------------------------------------
+class User{
+    email:string
+    name:string
+    constructor(email:string,name:string){
+        this.email =email;
+        this.name=name;
+    }
+}
+const myVar = new User("z@gmail.com" ,"dd")

d5363a29b170631a2dc498987370a393a3090c28
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-04 02:49:50 +0500

    word counts

---
+++ project05-word-counter/index.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const answer:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter your sentence "
+    }
+])
+ const words = answer.sentence.trim().split(" ")
+ console.log(` your sentence words length is ${words.length}`)
